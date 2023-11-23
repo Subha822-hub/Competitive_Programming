8afb6f89785a19bdaca214f842d31975d765f9ab
Subha822-hub <suseraj1974@gmail.com>
2023-11-23 22:29:34 +0530

    first commit

---
+++ CodingNinjas/Find_Character_Case.py
@@ -0,0 +1,7 @@
+x=input()
+if x.islower():
+    print(0)
+elif x.isupper():
+    print(1)
+else:
+    print(-1)
---
+++ CodingNinjas/if-else_(Decision Making).py
@@ -0,0 +1,10 @@
+from typing import *
+def compareIfElse(a: int, b: int):
+    # Write your code here
+    if a>b:
+        return "greater"
+    elif b>a:
+        return "smaller"
+    else:
+        return "equal"
+    pass
